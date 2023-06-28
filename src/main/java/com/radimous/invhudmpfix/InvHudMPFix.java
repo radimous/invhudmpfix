e12eb9e8c690945d870887b054490629185141df
radimous <jurda92@gmail.com>
2023-06-28 12:26:07 +0200

    v1.1 - added auto reloading of curios if it fails

--- src/main/java/com/radimous/invhudmpfix/InvHudMPFix.java
+++ src/main/java/com/radimous/invhudmpfix/InvHudMPFix.java
@@ -1,9 +1,12 @@
 package com.radimous.invhudmpfix;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Mod("invhudmpfix")
 public class InvHudMPFix {
+    public static final Logger logger = LogManager.getLogger("InvHudMPFix");
 }
 
--- src/main/java/com/radimous/invhudmpfix/mixins/CuriosRendererMixin.java
+++ src/main/java/com/radimous/invhudmpfix/mixins/CuriosRendererMixin.java
@@ -1,12 +1,18 @@
 package com.radimous.invhudmpfix.mixins;
 
+import com.radimous.invhudmpfix.InvHudMPFix;
 import dlovin.inventoryhud.gui.renderers.CuriosRenderer;
+import dlovin.inventoryhud.utils.CuriosSlot;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.TextComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
@@ -14,13 +20,24 @@ import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 import top.theillusivec4.curios.common.slottype.SlotType;
 import top.theillusivec4.curios.server.SlotHelper;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 @Mixin(CuriosRenderer.class)
-public class CuriosRendererMixin {
+public abstract class CuriosRendererMixin {
+    private long last = 0;
+    private int tries = 0;
+
+    @Shadow
+    public abstract HashMap<String, CuriosSlot> slots();
+
+    @Shadow
+    public abstract void setupTrinkets();
+
     @Inject(method = "setupTrinkets", at = @At("HEAD"))
     public void initCurios(CallbackInfo ci) {
+        tries++;
         if (CuriosApi.getSlotHelper() == null) {
             CuriosApi.setSlotHelper(new SlotHelper());
         }
@@ -39,8 +56,25 @@ public class CuriosRendererMixin {
                     }
                 }
             }
+            if(tries == 100  && CuriosApi.getSlotHelper().getSlotTypes().size() == 0){
+                pl.sendMessage(new TextComponent("[InvHudMPFix] Curio slots failed to load 100 times, remove this mod if your server doesn't support curios."), Util.NIL_UUID);
+            }
         }
+        if (CuriosApi.getSlotHelper().getSlotTypes().size() != 0) {
+            InvHudMPFix.logger.info("Loading curios took " + tries + " tries.");
+        }
+    }
 
+    @Inject(method = "isEmpty", at = @At("HEAD"))
+    public void isEmpty(CallbackInfoReturnable<Boolean> cir) {
+        if (slots().isEmpty() && tries < 100 && System.currentTimeMillis() - last > 3000) {
+            setupTrinkets();
+            last = System.currentTimeMillis();
+        }
+    }
+    @Inject(method = "disable", at = @At("HEAD"))
+    public void resetTries(CallbackInfo ci){
+        tries = 0;
     }
 
 }
